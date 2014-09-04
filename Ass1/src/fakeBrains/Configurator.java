2eca27939c9e774b560bf14249687c31c48f866b
Michael Ball <i.might.live@gmail.com>
2014-09-04 23:29:45 +1000

    Expanding Configurator

--- Ass1/src/fakeBrains/Configurator.java
+++ Ass1/src/fakeBrains/Configurator.java
@@ -9,26 +9,65 @@ import problem.*;
 
 public class Configurator {
 	
+	// The path through the map
+	List<Node> p;
+	
+	// The obstacles
+	List<Obstacle> o;
+	
+	// tester instance
+	Tester test;
+	
+	
+	/**
+	 * Default Constructor
+	 * Takes and sets up needed vars
+	 * @param path
+	 * @param obstacles
+	 */
+	public Configurator(List<Node> path, List<Obstacle> obstacles){
+		this.p = path;
+		this.o = obstacles;
+		test = new Tester();
+	}
+	
 	/**
 	 * Spider over all the nodes in path and create valid ASVConfigs
 	 * for each of them for Interpolate to use later
 	 * @param path
 	 * @return true if success
 	 */
-	public static boolean giveConfigurations(List<Node> path){
-		// Shortcut to the goal Node
-		Node goal = path.get(path.size()-1);
+	public boolean giveConfigurations(){
+		// Shortcut goal node
+		Node goal = p.get(p.size()-1);
+		
+		// counter
+		int i = 1;
 		
 		// can ignore the first node, as it is the starting node
-		Node current = path.get(1); 
-		while(current != goal){
+		Node current = p.get(i);
+		Node prev = p.get(0);
+		
+		// Time to Spider
+		while(prev != goal){
+			ASVConfig t = new ASVConfig(current.getPos(), prev.getConfig());
+
+			// If there is no collision at this node then there's no worry :D
+			// Just give current prev's shifted config
+			if(!test.hasCollision(t, o)){
+				current.giveASVConfig(new ASVConfig(t));
+				prev = current;
+				current = p.get(i++);
+				continue;
+			}
 			
+						
 			
+
 			
-			
-			
-			
-			
+			// Update things for the next round
+			prev = current;
+			current = p.get(i++);
 		}
 		return true;
 	}
