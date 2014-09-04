a6d15b6f06c4ba9f0e9cdc028252e78d8d0cc250
JHolland <jonathan.holland8@gmail.com>
2014-09-04 14:48:31 +1000

    Created framework/guide to work on for the interpolate class

--- Ass1/src/fakeBrains/Interpolate.java
+++ Ass1/src/fakeBrains/Interpolate.java
@@ -1,7 +1,47 @@
 package fakeBrains;
 
+import java.util.List;
+
 public class Interpolate {
 
+	/**
+	 * Moves from one node asv configuration to another,
+	 * changing the configuration shape as needed to meet
+	 * the end result and to avoid obstacles it comes near 
+	 * 
+	 * @param start - the node to begin from
+	 * @param end - the node to end at
+	 * @return the node of the end if successful
+	 */
+	public Node Interpolate(Node start, Node end) {
+		boolean asvLeft =  true;
+		
+		while(asvLeft) {
+			// While there are still asv's to check, add the start and end positions of each one to a map
+			
+		}
+		
+		// Use the start and end positions from the map to extract how many primitive steps each one needs to travel
+		
+		// Make a separate function to run through the movements WITH the constraints
+		// Make this separate function call another function on each iteration that checks constraints
+		// Same as above for the checking constrains but also checking for nearby obstacles
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		return end;
+	}
 	
 	
 }
