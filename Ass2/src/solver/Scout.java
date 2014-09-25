5ed0e2ba48d6663011d2660c95b54782f5bcc7c9
Michael-LAPTOP <i.might.live@gmail.com>
2014-09-25 11:49:02 +1000

    Started working on the 'scout' phase of our implementation (f we go along with what I'm thinking, but haven't explained to anyone yet)

---
+++ Ass2/src/solver/Scout.java
@@ -0,0 +1,27 @@
+package solver;
+
+import java.util.*;
+
+import problem.*;
+
+public class Scout {
+	
+	Map<Track, Integer> cyclesForTracks = new HashMap<Track, Integer>();
+	
+	/**
+	 * Something to initialise the class
+	 */
+	void Scout() {
+		
+	}
+	
+	/**
+	 * Iterate through all the possible tracks and decide 
+	 * what type of bike would be best for each one.
+	 * @param tracks
+	 */
+	void tracks(List<Track> tracks) {
+		
+	}
+	
+}
